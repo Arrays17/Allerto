a0820580ab12c11d3bad6d3c39cfb4235f30d1ce
Reggie Nald R. Reyes <reggienaldreyes17@gmail.com>
2022-03-07 22:47:57 +0800

    Current Location Showing and Call Function Added

--- components/listItems.js
+++ components/listItems.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Pressable, Linking, Platform } from 'react-native';
 
 export default ListItem = (props) => {
     return (
-        <Pressable>
+        <Pressable onPress={() => callNumber(props.number)} onLongPress={openModal}>
             <View style={styles.itemContainer}>
                 <Text style={styles.itemName}>{props.name}</Text>
                 <Text style={styles.itemDistance}>{props.distance.kilometers}km away</Text>
@@ -11,6 +11,33 @@ export default ListItem = (props) => {
             </View>
         </Pressable>
     )
+}
+
+function callNumber(number) {
+    console.log('Calling ' + number)
+    number = '(802) 287-9702' //for testing only, delete for production
+    let phoneNumber = ''
+    if (Platform.OS !== 'android'){
+        phoneNumber = `telprompt:${number}`
+    }
+    else {
+        phoneNumber = `tel:${number}`
+    }
+
+    Linking.canOpenURL(phoneNumber)
+        .then(supported => {
+            if (!supported) {
+                Alert.alert('Phone number is not available');
+            } 
+            else {
+                return Linking.openURL(phoneNumber);
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+function openModal() {
+    console.log('Open Modal')
 }
 
 const styles = StyleSheet.create({
