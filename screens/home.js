a398890fa51e219758d680605d99c5a5dd7bc4f6
Reggie Nald R. Reyes <reggienaldreyes17@gmail.com>
2022-03-11 15:11:56 +0800

    911 Button Added and Organized StyleSheet

--- screens/home.js
+++ screens/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import ServiceButton from '../components/service';
+import Button911 from '../components/btn_911';
 const s = require('../styles/styles');
 
 export default function Home({navigation}) {
@@ -30,6 +31,7 @@ export default function Home({navigation}) {
         {headerTitle: "Red Cross Hotlines"})} title="Red Cross" icon='hospital' />
       <ServiceButton onPress={()=>navigation.navigate("EmergencyList", 
         {headerTitle: "Disaster Hotlines"})} title="National Disaster" icon='bell' />
+      <Button911/>
     </View>
   );
 }
